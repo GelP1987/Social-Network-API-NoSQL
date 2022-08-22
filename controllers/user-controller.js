7964e90866deb41f82c01e80c0a2386e2f5b95b7
GelP1987 <101074657+GelP1987@users.noreply.github.com>
2022-08-21 20:38:02 -0400

    added thought controller and updated user conteoller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,81 +1,104 @@
-const { User } = require('../models');
+const { User, Thought } = require("../models");
 
 const userController = {
   getAllUsers: (req, res) => {
     User.find()
-      .select('-__v')
-      .then(users => res.json(users))
-      .catch(err => {
+      .select("-__v")
+      .then((data) => res.json(data))
+      .catch((err) => {
         console.log(err.message);
         return res.sendStatus(500);
       });
   },
   getSingleUser: (req, res) => {
-    const userId = req.params.id;
-
-    User.findOne({ _id: userId })
-      .populate('friends')
-      // .populate('thoughts')
-      .select('-__v')
-      .then(user => {
-        if (!user) {
-          return res.status(404).json({ message: 'User not found' });
+    User.findOne({ _id: req.params.userId })
+      .populate("friends")
+      .populate("thoughts")
+      .select("-__v")
+      .then((data) => {
+        if (!data) {
+          return res.status(404).json({ message: "User not found" });
         }
-
-        return res.json(user);
+        return res.json(data);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err.message);
         return res.sendStatus(500);
       });
   },
   createUser: (req, res) => {
-    const {
-      username,
-      email,
-    } = req.body;
-
+    const { username, email } = req.body;
     if (!username || !email) {
       return res.status(400).json({
-        message: 'Missing username or email',
+        message: "Missing username and/or email",
       });
     }
-
     User.create({ username, email })
-      .then(user => res.json(user))
-      .catch(err => {
+      .then((data) => res.json(data))
+      .catch((err) => {
         console.log(err.message);
         return res.sendStatus(500);
       });
   },
   updateUser: (req, res) => {
-    User.findOneAndUpdate(
-      { _id: req.params.id },
-      req.body,
-      { new: true, runValidators: true },
-    )
-      .then(user => {
-        if (!user) {
-          return res.status(404).json({ message: 'User not found' });
+    User.findOneAndUpdate({ _id: req.params.userId }, req.body, {
+      new: true,
+      runValidators: true,
+    })
+      .then((data) => {
+        if (!data) {
+          return res.status(404).json({ message: "User not found" });
         }
-
-        return res.json(user);
+        return res.json(data);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err.message);
         return res.sendStatus(500);
       });
   },
   deleteUser: (req, res) => {
-    User.findOneAndDelete({ _id: req.params.id })
-      .then(user => {
-        if (!user) {
-          return res.status(404).json({ message: 'User not found' });
+    User.findOneAndDelete({ _id: req.params.userId }).then((data) => {
+      if (!data) {
+        return res.status(404).json({ message: "User not found" });
+      }
+      return Thought.deleteMany({ _id: { $in: data.thoughts } });
+    });
+  },
+  addFriend: (req, res) => {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $push: { friends: req.params.friendId } },
+      {
+        new: true,
+        runValidators: true,
+      }
+    )
+      .then((data) => {
+        if (!data) {
+          res.status(404).json({ message: "User not found" });
         }
-
-        // TODO: Remove the user's `thoughts` records
-
-        return res.sendStatus(204);
+        res.json(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+        return res.sendStatus(500);
+      });
+  },
+  deleteFriend: (req, res) => {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $pull: { friends: req.params.friendId } },
+      { new: true }
+    )
+      .then((data) => {
+        if (!data) {
+          res.status(404).json({ message: "User not found1" });
+        }
+        res.json(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+        return res.sendStatus(500);
       });
   },
 };
